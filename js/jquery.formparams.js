69f134f2d4b2a25e31619c6247fc03b2979ed31e
Tom <tborychowski@gmail.com>
2012-10-02 11:36:30 +0100

    line was uncommented

--- js/jquery.formparams.js
+++ js/jquery.formparams.js
@@ -1,14 +1,14 @@
 /**
  * @author Tom
- * 
+ *
  * Based on: http://javascriptmvc.com/docs.html#!jQuery.fn.formParams
  *
- * 
+ *
  * GETs or SETs form parameters to/from Object.
- * 
- * When GET and convert == true -> strings that represent numbers and booleans will be converted and empty string will not be added to the object. 
+ *
+ * When GET and convert == true -> strings that represent numbers and booleans will be converted and empty string will not be added to the object.
  * When SET and convert == true -> fields which values are undefined (in the passed object) will be cleared
- * 
+ *
  * Example html:
  * @codestart html
  * &lt;form>
@@ -16,29 +16,29 @@
  *   &lt;input name="foo[ced]" value='4'/>
  * &lt;form/>
  * @codeend
- * 
- * Example code: 
- *     $('form').formParams() //-> { foo:{bar:'2', ced: '4'} } 
  *
- * 
- * @param {Object} [params] If an object is passed, the form will be repopulated with the values of the object based on the name of the inputs within the form
- * 
- * @param {Boolean} [convert=false] True if strings that look like numbers and booleans should be converted and if empty string should not be added to the result. Defaults to false.
- * 
+ * Example code:
+ *     $('form').formParams() //-> { foo:{bar:'2', ced: '4'} }
+ *
+ *
+ * @param {Object} params If an object is passed, the form will be re-populated with the values of the object based on the name of the inputs within the form
+ *
+ * @param {Boolean} convert (false) True if strings that look like numbers and booleans should be converted and if empty string should not be added to the result. Defaults to false.
+ *
  * @return {Object} An object of name-value pairs.
  */
 
-;(function($) {
-	var keyBreaker = /[^\[\]]+/g, 
+(function ($) {
+	var keyBreaker = /[^\[\]]+/g,
 		numberMatcher = /^[\-+]?[0-9]*\.?[0-9]+([eE][\-+]?[0-9]+)?$/,
-		isNumber = function(value) {
+		isNumber = function (value) {
 			if (typeof value == 'number') return true;
 			if (typeof value != 'string') return false;
 			return value.match(numberMatcher);
 		};
 
 	$.fn.extend({
-		formParams: function(params, convert) {
+		formParams: function (params, convert) {
 			if (typeof params === 'boolean') { convert = params; params = null; }
 			if (params) return this.setParams(params, convert);																	// SET
 			else if (this[0].nodeName.toLowerCase() == 'form' && this[0].elements) {											// GET
@@ -48,12 +48,12 @@
 		},
 
 
-		setParams: function(params, clear) {
-			this.find("[name]").each(function() {																				// Find all the inputs
+		setParams: function (params, clear) {
+			this.find("[name]").each(function () {																				// Find all the inputs
 				var name = $(this).attr("name"), value = params[name];
 
-				if (name.indexOf('[') > -1){																					// if name is object, e.g. user[name], userData[address][street], update value to read this correctly
-					var names = name.replace(/]/g, '').split('['), i = 0, n = null, v = params;
+				if (name.indexOf('[') > -1) {																					// if name is object, e.g. user[name], userData[address][street], update value to read this correctly
+					var names = name.replace(/\]/g, '').split('['), i = 0, n = null, v = params;
 					for (; n = names[i++] ;) if (v[n]) v = v[n]; else { v = undefined; break; }
 					value = v;
 				}
@@ -66,21 +66,21 @@
 				else {
 					if ("placeholder" in document.createElement("input")) this.value = value;									// normal browser
 					else {																										// manually handle placeholders for specIEl browser
-						var el=$(this); 
-						if (this.value != value && value != '') el.data('changed', true);
-						if (value == '') el.data('changed', false).val(el.attr('placeholder'));
-						else this.value = value;						
+						var el = $(this);
+						if (this.value != value && value !== '') el.data('changed', true);
+						if (value === '') el.data('changed', false).val(el.attr('placeholder'));
+						else this.value = value;
 					}
 				}
 			});
 		},
 
 
-		getParams: function(convert) {
+		getParams: function (convert) {
 			var data = {}, current;
 			convert = (convert === undefined ? false : convert);
 
-			this.each(function() {
+			this.each(function () {
 				var el = this, type = el.type && el.type.toLowerCase();
 				if ((type == 'submit') || !el.name)  return;																	// if we are submit, ignore
 				
@@ -90,12 +90,13 @@
 				if (el.type === 'radio' && !el.checked) return;																	// return only "checked" radio value
 				if (el.type === 'checkbox') value = el.checked;																	// convert chekbox to [true | false]
 
-				var $el=$(el); if($el.data('changed')!==true && value==$el.attr('placeholder')) value='';						// clear placeholder valus for IEs
+				var $el = $(el);
+				if ($el.data('changed') !== true && value === $el.attr('placeholder')) value = '';								// clear placeholder valus for IEs
 
-				if (convert){
-					if (isNumber(value)){
-						var tv = parseFloat(value), cmp=tv+'';
-						if (value.indexOf('.') > 0) cmp = tv.toFixed(value.split('.')[1].length);								// convert (string)100.00 to (int)100 
+				if (convert) {
+					if (isNumber(value)) {
+						var tv = parseFloat(value), cmp = tv + '';
+						if (value.indexOf('.') > 0) cmp = tv.toFixed(value.split('.')[1].length);								// convert (string)100.00 to (int)100
 						if (cmp === value) value = tv;
 					}
 					else if (value === 'true') value = true;
@@ -115,7 +116,7 @@
 						current[lastPart] = current[lastPart] === undefined ? [] : [current[lastPart]];
 					}
 					current[lastPart].push(value);
-				} 
+				}
 				else if (!current[lastPart]) current[lastPart] = value;
 			});
 			return data;
